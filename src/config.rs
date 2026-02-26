c367935acaa7f13ac210fafcf277ec5795786b7a
MVladhub <55245812+MVladhub@users.noreply.github.com>
2026-02-26 14:15:56 +0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.bit-erp.ru"];
+pub const RS_PUB_KEY: &str = "X1lwZBN8inxPdMh9viJxjRp2MFXJzJE1V4HF3Aqu0fE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
